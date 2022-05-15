cfd67d9ba514f8ea8ad2ea5927ab0b774bc103b2
yutabee <96982045+yutabee@users.noreply.github.com>
2022-05-15 23:42:25 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ChakraProvider } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+
+import theme from './theme/theme';
+import { Router } from './router/Router';
+
+export default function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ChakraProvider>
+  );
+}
